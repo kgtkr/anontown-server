56b18ca7f8f649ce38965b317859e253a6635993
kgtkr <contact@kgtkr.net>
2022-09-01 22:02:01 +0900

    依存整理

--- packages/server/src/types.d.ts
+++
@@ -1,4 +0,0 @@
-module "array-range" {
-  export default function range(a: number): Array<number>;
-  export default function range(a: number, b: number): Array<number>;
-}
--- packages/server/src/utils/index.ts
+++ packages/server/src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from "./hash";
 export * from "./mixin";
-export * from "./id-entity";
 export * from "./copyable";
 export * from "./string-validate";
 export * from "./observable-async-iterator";
