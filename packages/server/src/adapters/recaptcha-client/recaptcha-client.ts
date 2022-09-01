ec32b07dd8817a576d23ba4ee656be07a468eac3
kgtkr <contact@kgtkr.net>
2022-09-01 22:06:08 +0900

    request->axios

--- packages/server/src/adapters/recaptcha-client/recaptcha-client.ts
+++ packages/server/src/adapters/recaptcha-client/recaptcha-client.ts
@@ -1,30 +1,22 @@
-import * as request from "request";
+import axios from "axios";
 import { AtCaptchaError } from "../../at-error";
 import { Config } from "../../config";
 import { IRecaptchaClient } from "../../ports";
 
 export class RecaptchaClient implements IRecaptchaClient {
   async verify(apiParamRecaptcha: string): Promise<void> {
-    const result = await new Promise<string>((resolve, reject) => {
-      request.post(
-        "https://www.google.com/recaptcha/api/siteverify",
-        {
-          form: {
-            secret: Config.recaptcha.secretKey,
-            response: apiParamRecaptcha,
-          },
+    const result = await axios.post(
+      "https://www.google.com/recaptcha/api/siteverify",
+      undefined,
+      {
+        params: {
+          secret: Config.recaptcha.secretKey,
+          response: apiParamRecaptcha,
         },
-        (err, _res, body: string) => {
-          if (err) {
-            reject("キャプチャAPIでエラー");
-          } else {
-            resolve(body);
-          }
-        },
-      );
-    });
+      },
+    );
 
-    if (!JSON.parse(result).success) {
+    if (!(result.data as any).success) {
       throw new AtCaptchaError();
     }
   }
